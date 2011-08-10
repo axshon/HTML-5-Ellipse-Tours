f7c0a425390081bb25871333e74eae885c048558
Ian Gilman <ian@iangilman.com>
2011-08-10 14:14:10 -0700

    Canvas demo: + Added image, using transforms + Added text + More robust bounce testing

--- demos/canvas/README.md
+++ demos/canvas/README.md
@@ -1,9 +1,19 @@
 # TODO: 
 
-* Either handle resize better (for instance, make sure the lines are inside the new w/h, since our bounce logic is kind of lame) or just let it scale without explicitly resizing
-* Image drawing
-* Text drawing
-* Transforms
-* At least on Firefox, it never erases fully.
+* At least on Firefox, it never erases fully. Probably not a big deal
 * Test on various browsers
 * Use exCanvas or some such?
+* Use additional canvas features? See http://simon.html5.org/dump/html5-canvas-cheat-sheet.html
+* Should do something with direct pixel manipulation, at least
+
+# Comments from Jim:
+
+Any way to include more info related to images, shapes, text etc for Canvas? I was hoping that we could make each chapter's subject matter pretty definitive. Certainly not game-developer level but including as much of the current API implementation as possible.
+ 
+Perhaps a left panel that allows you to edit properties and add/edit elements on the canvas?
+
+# Notes
+
+We're free to use the smiley.png; it's from:
+
+http://findicons.com/icon/73377/smiley
--- demos/canvas/js/main.js
+++ demos/canvas/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   var $canvas = $("#main");
   var context = $canvas[0].getContext("2d");
   
+  var $image = $("<img src='img/smiley.png'>")
+    .hide()
+    .appendTo("body"); // we can't get the image's width and height without attaching to the DOM
+  
   var w; 
   var h;
   function resize() {  
@@ -9,10 +13,14 @@ $(document).ready(function() {
     h = $canvas.height();
     $canvas.attr("width", w);
     $canvas.attr("height", h);
-    context.fillStyle = "rgba(255, 255, 255, 0.1)";
+    
+    // resetting the width and height also resets these other properties, so we set them again
     context.lineWidth = 6;
     context.lineCap = "round";
     context.lineJoin = "round";
+    context.font = "100px sans-serif";  
+    context.textAlign = "center";
+    context.textBaseline = "middle";
   }
   
   $(window).resize(resize);
@@ -20,7 +28,7 @@ $(document).ready(function() {
   
   var maxVelocity = 10;
   var points = []; 
-  for (var a = 0; a < 3; a++) {
+  for (var a = 0; a < 5; a++) {
     points.push({
       x: Math.random() * w, 
       y: Math.random() * h,
@@ -29,28 +37,60 @@ $(document).ready(function() {
     });
   }
   
+  var radians = 0; // 2 * Math.PI is a complete circle
+  var text = "hello!";
+  
   function frame() {
+    // ___ clear
+    context.fillStyle = "rgba(255, 255, 255, 0.2)"; // 20% opaque white 
     context.fillRect(0, 0, w, h);
   
+    // ___ lines
     context.strokeStyle = "#0f0";
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    for (a = 1; a < points.length; a++) 
+    for (a = 1; a < 3; a++) 
       context.lineTo(points[a].x, points[a].y);
       
     context.stroke();
     
+    // ___ image
+    context.save();
+    context.translate(points[3].x, points[3].y);
+    context.rotate(radians);
+    context.drawImage($image[0], -($image.width()) / 2, -($image.height()) / 2);
+    context.restore();
+    
+    // ___ text
+    context.fillStyle = "#00a";
+    context.fillText(text, points[4].x, points[4].y); 
+    
+    // ___ update positions and handle bounce
     for (a = 0; a < points.length; a++) {
       var p = points[a];
+
       p.x += p.vx;
-      if (p.x < 0 || p.x > w)
+      if (p.x < 0) { 
+        p.x = 0;
         p.vx *= -1;
+      } else if (p.x > w) {
+        p.x = w;
+        p.vx *= -1;
+      }
         
       p.y += p.vy;
-      if (p.y < 0 || p.y > h)
+      if (p.y < 0) { 
+        p.y = 0;
         p.vy *= -1;
+      } else if (p.y > h) {
+        p.y = h;
+        p.vy *= -1;
+      }
     }
     
+    radians += 0.01;
+    
+    // ___ set up the next frame   
     setTimeout(frame, 30);
   }
   
