1be6e89631d1a09b469a4916ebf1d1e4f1f6887e
Ian Gilman <ian@iangilman.com>
2011-10-06 13:27:40 -0700

    + First draft of history client-side code

--- demos/history/MenuHistory/Scripts/main.js
+++ demos/history/MenuHistory/Scripts/main.js
@@ -1,5 +1,28 @@
 ﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
 
-$(document).ready(function () {
+// ----------
+function updateState(state) {
+  console.dir(state);
+}
 
+// ----------
+$(document).ready(function () {
+  $(window).bind("popstate", function(event) {
+    console.log("pop");
+    updateState(history.state);
+  });
+  
+  $(".menu-item").click(function(event) {
+    var $target = $(this);
+    var url = $target.find("a").attr("href");
+    var state = {
+      dish: url
+    };
+    history.pushState(state, "", url);
+    console.log("click");
+    updateState(state);
+    
+    event.preventDefault();
+    event.stopPropagation();
+  });
 });
