5e470998fdfd287c911182ef4ec3813dd9d05b10
Ian Gilman <ian@iangilman.com>
2011-10-13 11:09:56 -0700

    * Some clean up * Cool transition between previews * Add MIT license * Feature testing for graceful degradation * Deal properly with no dish states * show the right dish when you return to the initial state

--- demos/history/MenuHistory/Scripts/main.js
+++ demos/history/MenuHistory/Scripts/main.js
@@ -1,51 +1,83 @@
-﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
+﻿/// Copyright 2011, Ian Gilman
+/// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+///
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
+
+// ----------
+function stateFromPath(path) {
+  var base = "/Menu/";
+  var parts = path.replace(base, "").split("/");
+  return {
+    meal: (parts.length >= 1 ? parts[0] : ""),
+    dish: (parts.length >= 2 ? parts[1] : "")
+  };
+}
 
 // ----------
 function updateState(state) {
-  var $dish = $(".preview-dish");
+  if (!state)
+    state = stateFromPath(location.pathname);
   
-  if (!state || !state.dish) {
-    $dish.empty();
+  // ___ menu
+  var $selectedMenu = $(".menu[data-meal='" + state.meal + "']");
+  $(".menu").not($selectedMenu).removeClass("active");
+  $selectedMenu.addClass("active");
+  
+  var $selectedItem = $(".menu-item[data-dish='" + state.dish + "']");
+  $(".menu-item").not($selectedItem).removeClass("selected");
+  $selectedItem.addClass("selected");
+
+  // ___ preview    
+  var $dishContainer = $(".preview-dish");
+  var $oldDish = $dishContainer.find("section");
+  
+  if (!state.dish) {
+    $oldDish.fadeOut(function() {
+      $oldDish.remove();
+    });
+    
     return;
   }
     
   var url = "/Preview/Index/" + state.dish;
-  $dish.load(url, function() {
-  });
-  
-  $(".menu").each(function(index, menu) {
-    var $menu = $(menu);
-    if ($menu.data("meal") == state.meal)
-      $menu.addClass("active");
-    else
-      $menu.removeClass("active");
-  });
+  $.get(url, function(data) {
+    var $newDish = $(data);
 
-  $(".menu-item").each(function(index, item) {
-    var $item = $(item);
-    if ($item.data("dish") == state.dish)
-      $item.addClass("selected");
-    else
-      $item.removeClass("selected");
+    $newDish.find("img").load(function() {
+      $newDish.fadeIn(function() {
+        $newDish.css("z-index", 0);
+      });
+
+      $oldDish.fadeOut(function() {
+        $oldDish.remove();
+      });
+    });
+    
+    $newDish
+      .css("z-index", 1)
+      .hide()
+      .appendTo($dishContainer);
   });
 }
 
 // ----------
 $(document).ready(function () {
+  if (!Modernizr.history)
+    return;
+    
+  // ___ history event
   $(window).bind("popstate", function(event) {
     updateState(history.state);
   });
   
+  // ___ clicks
   $(".menu-item").click(function(event) {
     event.preventDefault();
     event.stopPropagation();
 
     var $target = $(this);
     var url = $target.find("a").attr("href");
-    var state = {
-      dish: $target.data("dish"),
-      meal: $target.parents(".menu").data("meal")
-    };
+    var state = stateFromPath(url);
 
     history.pushState(state, "", url);
     updateState(state);
--- demos/history/README.md
+++ demos/history/README.md
@@ -1,10 +1,4 @@
 # To Do
 
-* Cool transition between previews
-* Add MIT license
-* Try on other browsers
-* Feature testing for graceful degradation
 * jQuery history plugin?
 * Media query for mobile?
-* Deal properly with no dish states (and make it easier to get to them?)
-* show the right dish when you return to the initial state
