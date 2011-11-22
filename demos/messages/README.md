b1e47fdeb9da80fa9d9781270501a752a2e9bede
Ian Gilman <ian@iangilman.com>
2011-11-22 15:36:12 -0800

    + getting started on "messages" demo

---
+++ demos/messages/README.md
@@ -0,0 +1,11 @@
+# Welcome!
+
+Running demo: http://www.ellipsetours.com/Demos/messages/
+
+# To Do
+
+# Reference
+
+* http://stackoverflow.com/questions/1530023/do-i-need-a-server-to-use-html5s-websockets
+* http://stackoverflow.com/questions/4262543/what-are-good-resources-for-learning-html-5-websockets
+* https://developer.mozilla.org/en/DOM/window.postMessage
---
+++ demos/messages/Scripts/main.js
@@ -0,0 +1,51 @@
+﻿/// Copyright 2011, Ian Gilman
+/// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+///
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
+
+// ----------
+$(document).ready(function () {
+  Main.init();
+});
+
+// ----------
+window.Main = {
+  // ----------
+  init: function () {
+    var self = this;
+    
+    this.$output = $("#output");
+    
+    if (!Modernizr.postmessage) {
+      alert("This browser does not support web messaging");
+      return;
+    }
+    
+    if (!Modernizr.websockets) {
+      alert("This browser does not support web sockets");
+      return;
+    }
+    
+    $("#spawn").click(function() {
+      self.childWindow = window.open(location.href);
+    });
+    
+    this.$entry = $("#entry").keypress(function(event) {
+      if (event.which == 13) { // return key
+        var message = self.$entry.val();
+        self.$entry.val(""); 
+        self.$output.append("<p>" + message + "</p>");
+        if (self.childWindow)
+          self.childWindow.postMessage(message, location.href); 
+      }
+    });
+    
+    $(window).bind("message", function(event) {
+      if (event.originalEvent.origin != location.protocol + "//" + location.hostname)
+        return;
+
+      self.$output.append("<p>" + event.originalEvent.data + "</p>");
+    });
+  }
+};
+
