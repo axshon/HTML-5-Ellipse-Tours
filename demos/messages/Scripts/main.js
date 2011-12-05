8b0e36b2d3781d42e34f8f9a8c68b480ec391a6f
Ian Gilman <ian@iangilman.com>
2011-12-05 15:20:42 -0800

    Refactored around a server stub

--- demos/messages/Scripts/main.js
+++ demos/messages/Scripts/main.js
@@ -10,44 +10,103 @@ $(document).ready(function () {
 
 // ----------
 window.Main = {
-  name: "", 
+  user: null, 
+  members: [], 
   
   // ----------
-  init: function () {
+  init: function() {
     var self = this;
     
     this.$output = $("#log");
-    this.$users = $("#users");
+    this.$members = $("#members");
     
-    if (!Modernizr.websockets) {
-      alert("This browser does not support web sockets");
-      return;
-    }
+    this.$name = $("#name")
+      .keypress(function(event) {
+        if (event.which == 13) { // return key
+          var name = self.$name.val();
+          if (name) {
+            Server.send("connect", {
+              name: name
+            }, function(result) {
+              if (result && result.code == "success") {
+                self.user = self.addMember(result);
+                $("#login").hide();
+                $("#chat").show();
+                self.$entry.focus();
+              }
+            });
+          }
+        }
+      })
+      .focus();
     
     this.$entry = $("#entry")
       .keypress(function(event) {
         if (event.which == 13) { // return key
           var message = self.$entry.val();
           self.$entry.val(""); 
-          self.$output.append("<p>" + self.name + ": " + message + "</p>");
+          Server.send("message", {
+            memberID: self.user.id, 
+            message: message
+          });
         }
       });
-    
-    this.$name = $("#name")
-      .keypress(function(event) {
-        if (event.which == 13) { // return key
-          self.name = self.$name.val();
-          self.$users.append("<p>" + self.name + "</p>");
-          $("#login").hide();
-          $("#chat").show();
-          self.$entry.focus();
-        }
-      })
-      .focus();
+      
+    $(window).bind("beforeunload", function() {
+      if (self.user) {
+        Server.send("disconnect", {
+          memberID: self.user.id 
+        });
+      }
+    });
+      
+    Server.receive(function(method, data) { 
+      if (method == "memberEnter")
+        self.addMember(data);
+      else if (method == "memberExit")
+        self.removeMember(data);
+      else if (method == "message")
+        self.addMessage(data);
+    });
   },
   
   // ----------
-  message: function() {
+  addMember: function(data) {
+    var member = {
+      name: data.name, 
+      id: data.id
+    };
+    
+    member.$element = $("<p>" + member.name + "</p>")
+      .appendTo(this.$members);
+      
+    this.members.push(member);
+    return member;
+  },
+  
+  // ----------
+  deleteMember: function(data) {
+    var a; 
+    for (a = 0; a < this.members.length; a++) {
+      var member = this.members[a];
+      if (member.id == data.member) {
+        member.$element.remove();
+        this.members.splice(a, 1);
+        break;
+      }
+    }
+  },
+
+  // ----------
+  addMessage: function(data) {
+    var a; 
+    for (a = 0; a < this.members.length; a++) {
+      var member = this.members[a];
+      if (member.id == data.memberID) {
+        this.$output.append("<p>" + member.name + ": " + data.message + "</p>");
+        break;
+      }
+    }
   }
 };
 
---
+++ demos/messages/Scripts/server.js
@@ -0,0 +1,35 @@
+﻿/// Copyright 2011, Ian Gilman
+/// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+///
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
+
+// ----------
+window.Server = {
+  callback: null, 
+  nextID: 1, 
+    
+  // ----------
+  receive: function(callback) {
+    this.callback = callback;
+
+/*
+    if (!Modernizr.websockets) {
+      alert("This browser does not support web sockets");
+      return;
+    }
+*/
+  },
+  
+  // ----------
+  send: function(method, data, complete) {
+    if (method == "connect") {
+      data.id = this.nextID;
+      this.nextID++;
+      data.code = "success";
+      complete(data);
+    } else if (method == "message") {
+      this.callback("message", data);
+    }
+  }
+};
+
