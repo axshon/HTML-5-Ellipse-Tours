535d311501b7fcf436e81344846babc2f04fccca
Ian Gilman <ian@iangilman.com>
2012-01-25 10:48:57 -0800

    First version of AV demo

---
+++ demos/av/README.md
@@ -0,0 +1,18 @@
+# Welcome!
+
+Running demo: http://www.ellipsetours.com/Demos/av/
+
+# To Do
+
+* Find suitable audio and video to use
+* Our own controls
+
+# Reference
+
+* https://developer.mozilla.org/en/Using_HTML5_audio_and_video
+* http://www.w3schools.com/html5/html5_audio.asp
+* http://www.worldwidewhat.net/extras/html5-audio-converter/
+* http://www.mirovideoconverter.com/
+* http://www.w3schools.com/html5/html5_video.asp
+* http://www.html5rocks.com/en/tutorials/video/basics/
+* https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIDOMHTMLMediaElement
---
+++ demos/av/Scripts/main.js
@@ -0,0 +1,53 @@
+﻿/// Copyright 2012, Ian Gilman
+/// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+///
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.3.js" />
+
+// ----------
+$(document).ready(function () {
+  Main.init();
+});
+
+// ----------
+window.Main = {
+  // ----------
+  init: function() {
+    var self = this;
+    
+    if (!Modernizr.audio) {
+      alert("This browser does not support the audio tag.");
+      return;
+    }
+
+    if (!Modernizr.video) {
+      alert("This browser does not support the video tag.");
+      return;
+    }
+    
+    this.$play = $("#play")
+      .click(function() {
+        if (self.video.paused)
+          self.video.play();
+        else
+          self.video.pause();
+      });
+      
+    this.$video = $("#video1")
+      .bind("playing", function() {
+        self.$play.text("pause");
+      })
+      .bind("pause", function() {
+        self.$play.text("play");
+      });
+      
+    this.video = this.$video[0];
+    
+/*
+	<button onclick="document.getElementById('player').play()">Play</button>
+	<button onclick="document.getElementById('player').pause()">Pause</button>
+	<button onclick="document.getElementById('player').volume+=0.1">Volume Up</button>
+	<button onclick="document.getElementById('player').volume-=0.1">Volume Down</button>
+*/
+  }
+};
+
